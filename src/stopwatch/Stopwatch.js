527939aa9f9475702217f11687cbd86fd10bf734
ninad-react <ninadsamant@nimapinfotech.com>
2024-01-27 17:34:54 +0530

    hours-mins-sec-milisec logic

--- src/stopwatch/Stopwatch.js
+++ src/stopwatch/Stopwatch.js
@@ -1,13 +1,52 @@
+import { useState,  useEffect } from "react";
+
 import "./style.css";
 
 const Stopwatch = () => {
+
+    const[time, setTime] = useState(0);
+    const[isRunning, setIsRunning] = useState(false);
+
+    // to calculate milliseconds
+    const milliseconds = time % 100;
+
+    //to calculate seconds
+    const seconds = Math.floor((time % 6000) / 100);
+    
+    //to calculate minutes
+    const minutes = Math.floor((time %  3600000) / 6000);
+
+    //to calculate hours
+    const hours = Math.floor(time / 3600000);
+
+    useEffect(() => {
+        let interval;
+
+        if(isRunning){
+            interval = setInterval(() => setTime(time + 1), 10);
+        }
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, [time, isRunning]);
+
+    const handleStartAndStop = () => {
+        setIsRunning(!isRunning);
+    };
+
+    const handleReset = () => {
+        setTime(0);
+    }
+
     return (
     <>
         <h1 className="heading">Stopwatch with React</h1>
-        <p className="showTime">hours:minutes:seconds</p>
+        <p className="showTime">{hours.toString().padStart(2, "0")}:{minutes.toString().padStart(2, "0")}:{seconds.toString().padStart(2, "0")}:{milliseconds.toString().padStart(2, "0")}</p>
         <div className="buttons">
-            <button className="btn">Start</button>
-            <button className="btn">Reset</button>
+            <button className="btn" onClick={handleStartAndStop}>
+                {isRunning ? "Stop" : "Start"}</button>
+            <button className="btn" onClick={handleReset}>Reset</button>
         </div>
     </>
 )
